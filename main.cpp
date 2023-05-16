a717e7e40528390379db12f57d38fccfee1dbf89
glucogen19 <98939682+glucogen19@users.noreply.github.com>
2023-05-16 08:05:59 +0400

    5) Создание конструктора без параметров

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
-
 class Car {
-    private:
-   	 double X;
-   	 int P;
-   	 double V;
+	private:
+		double X;
+		int P;
+		double V;
+	public:
+		Car(){
+			X = 0;
+			P = 0;
+			V = 0;
+		}
 };
 
 int main() {}
