da226833787b894cb206845e002a9454d15b0f93
glucogen19 <98939682+glucogen19@users.noreply.github.com>
2023-05-16 08:07:46 +0400

    9) Создание второго объекта и вывод значений

--- main.cpp
+++ main.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
 using namespace std;
 class Car {
-	private:
-		double X;
-		int P;
-		double V;
-	public:
-		Car(){
-			X = 0;
-			P = 0;
-			V = 0;
-		}
-		Car(double A, int B, double C) {
-			X = A;
-			P = B;
-			V = C;
-		}
-		void out() {	
-			cout << X << P << V;
-		}
+private:
+	double X;
+	int P;
+	double V;
+public:
+	Car() {
+		X = 0;
+		P = 0;
+		V = 0;
+	}
+	Car(double A, int B, double C) {
+		X = A;
+		P = B;
+		V = C;
+	}
+	void out() {
+		cout << X << P << V;
+		cout << X << ' ' << P << ' ' << V << '\n';
+	}
 };
 
 int main() {
 	Car Alpha;
+	cout << "Alpha: ";
 	Alpha.out();
+	Car Beta(1, 2, 3);
+	cout << "Beta:  ";
+	Beta.out();
 }
